3ca8ef61f0a9d3ac5830892874d81b51c503cf6a
Karan Kalra <ebook@karankalra.in>
2022-02-26 00:24:10 +0530

    updated environment file with node ip and updated accounts.json

--- elastprepbook/elksetup.sh
+++ elastprepbook/elksetup.sh
@@ -25,7 +25,14 @@ sudo chmod 777 /mnt/backup
 sudo /usr/local/bin/docker-compose -f cluster01.yml -p cluster01 up -d
 sudo /usr/local/bin/docker-compose -f cluster02.yml -p cluster02 up -d
 
-node_ip=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+NODE_IP=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 
-echo "ekcluster01 Kibana GUI is accessible at $node_ip:5601"
-echo "ekcluster02 Kibana GUI is accessible at $node_ip:5602"
+NODE_IP=192.168.193.177
+
+grep -q '^NODE_IP=' .env || echo 'NODE_IP=VALUE' >> .env
+sed -i 's/NODE_IP=.*$/'NODE_IP="$NODE_IP"'/' .env
+
+#grep -qxF 'NODE_IP=$NODE_IP' .env || echo 'NODE_IP='$NODE_IP >> .env
+
+echo "ekcluster01 Kibana GUI is accessible at $NODE_IP:5601"
+echo "ekcluster02 Kibana GUI is accessible at $NODE_IP:5602"
